595d825cb4239576a87da6eeba78f10a40e0b653
jdenkim <125925293+jdenkim@users.noreply.github.com>
2023-02-22 15:46:45 -0500

    09App

---
+++ MarsProject/Assets/ARSceneManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ARWT.Core
+{
+    public class ARSceneManager : MonoBehaviour
+    {
+        public void GoToMain()
+
+        {
+            SceneManager.LoadScene(0, LoadSceneMode.Single)
+    ;
+        }
+
+        public void GoToScene(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+        }
+    }
+
+}
